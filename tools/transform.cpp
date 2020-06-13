65e8bef2515dba69c586097f7250a4d0eba253f9
Nuno Lopes <nuno.lopes@ist.utl.pt>
2020-06-13 10:34:13 +0100

    Update transform.cpp

--- tools/transform.cpp
+++ tools/transform.cpp
@@ -200,12 +200,12 @@ static expr preprocess(Transform &t, const set<expr> &qvars0,
   for (auto &var : qvars0) {
     if (!var.isBool())
       continue;
+    if (hit_half_memory_limit())
+      break;
+
     e = e.subst(var, true).simplify() &&
         e.subst(var, false).simplify();
     qvars.erase(var);
-
-    if (hit_half_memory_limit())
-      break;
   }
 
   // TODO: maybe try to instantiate undet_xx vars?
