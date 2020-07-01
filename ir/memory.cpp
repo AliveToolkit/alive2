14db7b3787b613650548645e93f79ef7f6d3fdd9
Nuno Lopes <nuno.lopes@ist.utl.pt>
2020-07-01 11:36:48 +0100

    acess: simplify encoding of cond

--- ir/memory.cpp
+++ ir/memory.cpp
@@ -1050,13 +1050,18 @@ void Memory::access(const Pointer &ptr, unsigned bytes, unsigned align,
 
   expr is_local = ptr.isLocal();
 
-  expr bid = ptr.getShortBid();
+  bool has_both = has_local && has_nonlocal;
+  bool is_singleton = has_local + has_nonlocal == 1;
+  expr bid = has_both ? ptr.getBid() : ptr.getShortBid();
+  unsigned bits_bid = bits_shortbid();
+  expr one = expr::mkUInt(1, 1);
+
   for (unsigned i = 0, e = access_local.size(); i < e; ++i) {
     if (access_local[i]) {
       auto &[mem, undef] = local_block_val[i];
+      auto n = expr::mkUInt(i, bits_bid);
       fn(const_cast<expr&>(mem), const_cast<set<expr>&>(undef), i, true,
-         (has_nonlocal ? is_local : true) &&
-         (has_local == 1 ? true : bid == i));
+         is_singleton ? true : bid == (has_both ? one.concat(n) : n));
     }
   }
 
@@ -1064,8 +1069,7 @@ void Memory::access(const Pointer &ptr, unsigned bytes, unsigned align,
     if (access_nonlocal[i]) {
       auto &[mem, undef] = non_local_block_val[i];
       fn(const_cast<expr&>(mem), const_cast<set<expr>&>(undef), i, false,
-         (has_local ? !is_local : true) &&
-         (has_nonlocal == 1 ? true : bid == i));
+         is_singleton ? true : bid == i);
     }
   }
 }
