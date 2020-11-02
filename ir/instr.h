f0cac220b55a8961688c6de6306549e203b67862
Nuno Lopes <nuno.lopes@ist.utl.pt>
2020-11-02 16:41:44 +0000

    Update instr.h

--- ir/instr.h
+++ ir/instr.h
@@ -294,7 +294,7 @@ public:
 
   void addValue(Value &val, std::string &&BB_name);
   void removeValue(const std::string &BB_name);
-  void replaceSourceWith(const std::string &From, const std::string &To);
+  void replaceSourceWith(const std::string &from, const std::string &to);
   std::vector<std::string> sources() const;
 
   std::vector<Value*> operands() const override;
