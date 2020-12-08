24cdb7e28d1a39ae629ff00b153c5e4f06fb4e49
Nuno Lopes <nuno.lopes@ist.utl.pt>
2020-12-08 10:49:21 +0000

    Update parallel_unrestricted.cpp

--- util/parallel_unrestricted.cpp
+++ util/parallel_unrestricted.cpp
@@ -1,19 +1,11 @@
 // Copyright (c) 2018-present The Alive2 Authors.
 // Distributed under the MIT license that can be found in the LICENSE file.
 
-#include "util/compiler.h"
 #include "util/parallel.h"
-#include <cassert>
 #include <cstdlib>
-#include <cstring>
-#include <fcntl.h>
-#include <iostream>
-#include <sys/wait.h>
-#include <unistd.h>
 
-bool unrestricted::init(int _max_subprocesses) {
-  parallel::init(_max_subprocesses);
-  return true;
+bool unrestricted::init(int max_subprocesses) {
+  return parallel::init(max_subprocesses);
 }
 
 std::tuple<pid_t, std::ostream *, int> unrestricted::limitedFork() {
