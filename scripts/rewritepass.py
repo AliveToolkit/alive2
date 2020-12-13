be06ce3f9440ac24575b187127d30bdc6b0477ad
Juneyoung Lee <aqjune@gmail.com>
2020-12-14 07:57:05 +0900

    use PassRegistry.def

--- scripts/rewritepass.py
+++ scripts/rewritepass.py
@@ -3,14 +3,16 @@ import sys
 import os
 
 if len(sys.argv) != 3:
-  print("Use: %s <opt path> <passes>" % sys.argv[0])
+  print("Use: %s <PassRegistry.def path> <passes>" % sys.argv[0])
   exit(1)
 
-optpath = sys.argv[1]
+passregpath = sys.argv[1]
 passes = sys.argv[2].strip("'\"").split(',')
 plen = len(passes)
 
+levels = ["module", "cgscc", "function", "loop"]
 level_ancestors = {
+  "module":[],
   "cgscc":["module"],
   "function":["module"],
       # don't use module(cgscc(function(...)), use module(function(...))
@@ -24,64 +26,54 @@ PassBuilder.cpp states that:
   As a special shortcut, if the very first pass is not
   a module pass (as a module pass manager is), this will automatically form
   the shortest stack of pass managers that allow inserting that first pass.
-Build the stack of pass managers by invoking opt with the first pass and
-parsing its error message.
+Build the stack of pass managers by finding the first pass from PassRegistry.def
 """
-i = 0
-firstp = passes[i]
-if firstp.find("<") != -1 or firstp.find("(") != -1:
-  # make p complete by concatenating tokens until the corresponding ">"
-  # and ")" are found
-  par = par2 = 0
-  ps = []
-  while i < plen:
-    ps.append(passes[i])
-    par  += passes[i].count("(") - passes[i].count(")")
-    par2 += passes[i].count("<") - passes[i].count(">")
-    if par == 0 and par2 == 0:
-      break
-    i = i + 1
-  firstp = ",".join(ps)
 
+firstp_level = None
+# If the level is explicitly given, use it
+for k in levels:
+  if passes[0].startswith(k + "("):
+    firstp_level = k
+    break
 
-# 'opt -passes=..' does not generate the message we want if one of these passes
-# is used.
-unsupported_module_passes = [
-  "function-import",
-  "pgo-instr-use",
-  "sample-profile"
-]
-
-firstp_level = "module"
-if firstp not in unsupported_module_passes:
-  # get the level of the first pass by invoking opt and parsing the output
-  # message.
-  optparam = '-passes="%s,module(globalopt)" -disable-output 2>&1' % firstp
-  output = os.popen('echo "" | "%s" %s' %
-                    (optpath, optparam)).read()
-  lines = output.strip().split("\n")
-  assert(len(lines) == 1)
-  if lines[0] != "":
-    msg = "invalid use of 'module' pass as ";
-    assert(lines[0].find(msg) != -1), \
-          "unknown output from opt: " + lines[0] + ", optparam: " + optparam
-
-    # level_str becomes e.g. "function pipeline"
-    level_str = lines[0][lines[0].find(msg) + len(msg):]
-    # get "function"
-    firstp_level = level_str.split()[0]
-    assert(firstp_level in level_ancestors)
-  else:
+for i in ["s", "z", "0", "1", "2", "3"]:
+  if passes[0] == "default<O" + i + ">":
     firstp_level = "module"
+    break
 
-i = i + 1
-res = [firstp] + passes[i:]
+if firstp_level == None:
+  # ask PassRegistry.def
+  firstpass = passes[0]
+  if passes[0].startswith("require<"):
+    firstpass = passes[0][len("require<"):]
+  firstpass = firstpass.replace("(", ",").replace("<",",").replace(">",",") \
+                       .replace(";", ",").split(",")[0]
+
+  # There are passes like "verify", which can be either function or module
+  # level. We should allow them
+  found_levels = dict()
+  prefixes = ["MODULE_", "CGSCC_", "FUNCTION_", "LOOP_"]
+  for l in open(passregpath, "r").readlines():
+    if l.find(firstpass) == -1:
+      continue
+    for i in range(0, len(prefixes)):
+      if l.startswith(prefixes[i]):
+        found_levels[levels[i]] = True
+
+  if "module" in found_levels and len(found_levels) == 1:
+    # precisely a module level pass
+    firstp_level = "module"
+  else:
+    for i in range(1, len(prefixes)): # can be non-module (ex: verify)
+      if levels[i] in found_levels:
+        firstp_level = levels[i]
+        break
 
 prefix = ""
 suffix = ""
 
 if firstp_level != "module":
-  if not firstp.startswith(firstp_level):
+  if not passes[0].startswith(firstp_level):
     # don't print "function(function(passes))"
     prefix = firstp_level + "("
     suffix = ")"
@@ -91,4 +83,4 @@ if firstp_level != "module":
     suffix = suffix + ")"
 
 
-print(firstp_level + " " + prefix + ",".join(res) + suffix)
+print(firstp_level + " " + prefix + ",".join(passes) + suffix)
