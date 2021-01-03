de0935d92f1c485b100868a488914c8e9f1fe2ae
Nuno Lopes <nuno.lopes@ist.utl.pt>
2021-01-03 14:33:55 +0000

    typo

--- ir/state.h
+++ ir/state.h
@@ -72,7 +72,7 @@ private:
 
     VarArgsEntry() {}
     VarArgsEntry(smt::expr &&alive, smt::expr &&next_arg, smt::expr &&num_args,
-                 smt::expr &&va_start, smt::expr &&active)
+                 smt::expr &&is_va_start, smt::expr &&active)
       : alive(std::move(alive)), next_arg(std::move(next_arg)),
         num_args(std::move(num_args)), is_va_start(std::move(is_va_start)),
         active(std::move(active)) {}
