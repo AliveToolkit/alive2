cd9c07ebd13aa0ce6c667fabb66f991013188bff
Nuno Lopes <nuno.lopes@ist.utl.pt>
2021-03-28 20:45:58 +0100

    test github actions

---
+++ .github/scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+      -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER \
+      -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS \
+      -GNinja ..
+ninja
