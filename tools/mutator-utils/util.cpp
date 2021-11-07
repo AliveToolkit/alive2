1fbf9ef9b1a6bf32767ce516ee7c4832d21e4b14
spica <Spica>
2021-11-06 21:24:14 -0700

    Module iterator added.

--- tools/mutator-utils/util.cpp
+++ tools/mutator-utils/util.cpp
@@ -4,3 +4,45 @@ std::random_device Random::rd;
 std::uniform_int_distribution<int> Random::dist(0,2147483647u);
 unsigned Random::seed(rd());
 std::mt19937 Random::mt(Random::seed);
+
+#define pm fit->getParent()
+void ModuleIterator::moveToNextInstruction(){
+    ++fit;
+    ++functionDistance;
+    if(fit==pm->end()){
+        fit=pm->begin();
+        functionDistance=0;
+    }
+    while(fit->isDeclaration()){
+        ++fit;
+        ++functionDistance;
+        if(fit==pm->end()){
+            fit=pm->begin();
+            functionDistance=0;
+        }
+    }
+    bit=fit->begin();
+    basicBlockDistance=0;
+    iit=bit->begin();
+    instructionDistance=0;
+}
+#undef pm
+
+void ModuleIterator::moveToNextBasicBlock(){
+    ++bit;
+    ++basicBlockDistance;
+    if(bit==fit->end()){
+        moveToNextFuction();
+    }else{
+        iit=bit->begin();
+        instructionDistance=0;
+    }
+}
+
+void ModuleIterator::moveToNextFuction(){
+    ++iit;
+    ++instructionDistance;
+    if(iit==bit->end()){
+        moveToNextBasicBlock();
+    }
+}
--- tools/mutator-utils/util.h
+++ tools/mutator-utils/util.h
@@ -5,6 +5,27 @@
 #include <memory>
 #include <random>
 #include <ctime>
+#include "llvm/IR/Module.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/Triple.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/Bitcode/BitcodeReader.h"
+#include "llvm/InitializePasses.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/Error.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/IR/Verifier.h"
 
 using std::string;
 
@@ -22,4 +43,35 @@ class Random{
       seed=seed_;
       mt=std::mt19937(seed);
     }
+};
+
+class ModuleIterator{
+llvm::Module::iterator fit;
+llvm::Function::iterator bit;
+llvm::BasicBlock::iterator iit;
+int functionDistance,basicBlockDistance,instructionDistance;
+public:
+  ModuleIterator(llvm::Module::iterator fit, 
+                 llvm::Function::iterator bit,
+                 llvm::BasicBlock::iterator iit):fit(fit),bit(bit),iit(iit){
+                   functionDistance=basicBlockDistance=instructionDistance=0;
+                 }
+  ModuleIterator(llvm::Module* pm,const ModuleIterator& mit){
+    fit=pm->begin();
+    functionDistance=mit.functionAdvanced();
+    for(int i=0;i<functionDistance;++i)++fit;
+    bit=fit->begin();
+    basicBlockDistance=mit.basicBlockAdvanced();
+    for(int i=0;i<basicBlockDistance;++i)++bit;
+    iit=bit->begin();
+    instructionDistance=mit.instructionAdvanced();
+    for(int i=0;i<instructionDistance;++i)++iit;
+  }
+  ~ModuleIterator(){};
+  int functionAdvanced()const{return functionDistance;}
+  int basicBlockAdvanced()const{return basicBlockDistance;}
+  int instructionAdvanced()const{return instructionDistance;}
+  void moveToNextInstruction();
+  void moveToNextBasicBlock();
+  void moveToNextFuction();
 };
