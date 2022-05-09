cfd47b45ef1bc1ee619a8f4ad1d2f14d73b2b126
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2022-05-09 19:37:43 +0100

    fix unit test

--- tests/clang-tv/issue579.c
+++ tests/clang-tv/issue579.c
@@ -1,7 +1,7 @@
 // TEST-ARGS: -O3 -mllvm -tv-tgt-unroll=2 -mllvm -tv-src-unroll=2 -mllvm -tv-smt-to=10000 -mllvm -tv-disable-undef-input -mllvm -tv-disable-poison-input
 short *d;
 
-e(int a, int b, int c) {
+void e(int a, int b, int c) {
   for (int f = 0; f < b; f += c)
     a = d[c];
 }
