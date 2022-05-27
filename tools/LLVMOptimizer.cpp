8a1389863fb4a1365cbe7e6f2a9ce2d9d465b813
spica <Spica>
2022-05-27 16:18:57 -0700

    API changed

--- tools/LLVMOptimizer.cpp
+++ tools/LLVMOptimizer.cpp
@@ -2,8 +2,15 @@
 
 using namespace llvm;
 
-LLVMOptimizer::LLVMOptimizer(std::string optArgs) {
-  this->optArgs = optArgs;
+llvm::Module *optimize_module(std::string optArgs, llvm::Module *M) {
+  llvm::LoopAnalysisManager LAM;
+  llvm::FunctionAnalysisManager FAM;
+  llvm::CGSCCAnalysisManager CGAM;
+  llvm::ModuleAnalysisManager MAM;
+  llvm::PassBuilder PB;
+
+  llvm::ModulePassManager MPM;
+
   PB.registerModuleAnalyses(MAM);
   PB.registerCGSCCAnalyses(CGAM);
   PB.registerFunctionAnalyses(FAM);
@@ -27,14 +34,42 @@ LLVMOptimizer::LLVMOptimizer(std::string optArgs) {
       llvm::errs() << E << "\n";
     }
   }
-}
-
-llvm::Module *LLVMOptimizer::optimizeModule(llvm::Module *M) {
   MPM.run(*M, MAM);
   return M;
 }
 
-llvm::Function *LLVMOptimizer::optimizeFunction(llvm::Function *func) {
+llvm::Function *optimize_function(std::string optArgs, llvm::Function *func) {
+  llvm::LoopAnalysisManager LAM;
+  llvm::FunctionAnalysisManager FAM;
+  llvm::CGSCCAnalysisManager CGAM;
+  llvm::ModuleAnalysisManager MAM;
+  llvm::PassBuilder PB;
+
+  llvm::ModulePassManager MPM;
+
+  PB.registerModuleAnalyses(MAM);
+  PB.registerCGSCCAnalyses(CGAM);
+  PB.registerFunctionAnalyses(FAM);
+  PB.registerLoopAnalyses(LAM);
+  PB.crossRegisterProxies(LAM, FAM, CGAM, MAM);
+
+  if (optArgs == "O3") {
+    MPM = PB.buildPerModuleDefaultPipeline(OptimizationLevel::O3);
+  } else if (optArgs == "O2") {
+    MPM = PB.buildPerModuleDefaultPipeline(OptimizationLevel::O2);
+  } else if (optArgs == "O1") {
+    MPM = PB.buildPerModuleDefaultPipeline(OptimizationLevel::O1);
+  } else if (optArgs == "O0") {
+    MPM = PB.buildPerModuleDefaultPipeline(OptimizationLevel::O0);
+  } else if (optArgs == "Os") {
+    MPM = PB.buildPerModuleDefaultPipeline(OptimizationLevel::Os);
+  } else if (optArgs == "Oz") {
+    MPM = PB.buildPerModuleDefaultPipeline(OptimizationLevel::Oz);
+  } else {
+    if (auto E = PB.parsePassPipeline(MPM, optArgs)) {
+      llvm::errs() << E << "\n";
+    }
+  }
   MPM.run(*(func->getParent()), MAM);
   return func;
 }
--- tools/LLVMOptimizer.h
+++ tools/LLVMOptimizer.h
@@ -1,52 +1,14 @@
 #pragma once
-#include "llvm/ADT/StringExtras.h"
-#include "llvm/ADT/Triple.h"
-#include "llvm/Analysis/MemorySSA.h"
-#include "llvm/Analysis/TargetLibraryInfo.h"
-#include "llvm/Bitcode/BitcodeReader.h"
-#include "llvm/IR/CFG.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/InstIterator.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Verifier.h"
-#include "llvm/IRReader/IRReader.h"
-#include "llvm/InitializePasses.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Support/Error.h"
-#include "llvm/Support/FormatVariadic.h"
-#include "llvm/Support/PrettyStackTrace.h"
-#include "llvm/Support/Signals.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-#include "llvm/Transforms/Scalar/LICM.h"
-#include "llvm/Transforms/Scalar/NewGVN.h"
 
-#include <cassert>
-#include <climits>
-#include <ctime>
-#include <memory>
-#include <random>
 #include <string>
-#include <vector>
 
-class LLVMOptimizer {
-  std::string optArgs;
-  llvm::LoopAnalysisManager LAM;
-  llvm::FunctionAnalysisManager FAM;
-  llvm::CGSCCAnalysisManager CGAM;
-  llvm::ModuleAnalysisManager MAM;
-  llvm::PassBuilder PB;
 
-  llvm::FunctionPassManager FPM;
-  llvm::ModulePassManager MPM;
-
-public:
-  LLVMOptimizer(std::string optArgs);
-  llvm::Module *optimizeModule(llvm::Module *M);
-  llvm::Function *optimizeFunction(llvm::Function *func);
-  const std::string &getOptArgs() const {
-    return optArgs;
-  }
-};
+llvm::Module *optimize_module(std::string optArgs,llvm::Module *M);
+llvm::Function *optimize_function(std::string optArgs, llvm::Function *func);
