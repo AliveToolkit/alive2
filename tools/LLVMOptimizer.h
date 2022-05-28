10430c8a4380314e6abffa4f0fc745e6c240cae9
spica <Spica>
2022-05-28 14:35:59 -0700

    return type fixed. Extra header removed, function impl rewritten.

--- tools/LLVMOptimizer.h
+++ tools/LLVMOptimizer.h
@@ -1,14 +1,5 @@
-#pragma once
-#include "llvm/IR/Function.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Verifier.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Support/Error.h"
 
-#include <string>
-
-
-llvm::Module *optimize_module(std::string optArgs,llvm::Module *M);
-llvm::Function *optimize_function(std::string optArgs, llvm::Function *func);
+void optimize_module(llvm::StringRef optArgs,llvm::Module *M);
+void optimize_function(llvm::StringRef optArgs, llvm::Function *func);
