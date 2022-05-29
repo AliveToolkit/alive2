dfeb9d0127ba67f542229bbd5d3b3d1a47e9ea14
spica <Spica>
2022-05-29 11:01:24 -0700

    move LLVMOptimizer from tool to llvm_util

--- tools/alive-tv.cpp
+++ tools/alive-tv.cpp
@@ -2,9 +2,9 @@
 // Distributed under the MIT license that can be found in the LICENSE file.
 
 #include "llvm_util/llvm2alive.h"
+#include "llvm_util/LLVMOptimizer.h"
 #include "smt/smt.h"
 #include "tools/transform.h"
-#include "tools/LLVMOptimizer.h"
 #include "util/version.h"
 
 #include "llvm/ADT/StringExtras.h"
