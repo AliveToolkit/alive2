4e5826c70c8b4c17cc28c07b9a41fb5c84daa6fa
John Regehr <regehr@cs.utah.edu>
2023-01-03 19:44:29 -0700

    reimplemented backend-tv

--- backend_tv/mc.h
+++ backend_tv/mc.h
@@ -3,7 +3,7 @@
 #include "llvm/IR/Module.h"
 
 std::pair<llvm::Function *, llvm::Function *>
-  lift_func(llvm::Module &ArmModule, llvm::Module &LiftedModule,
+  lift_func(llvm::Module &OrigModule, llvm::Module &LiftedModule,
             bool asm_input, std::string opt_file2,
             bool opt_asm_only,
             llvm::Function *srcFnLLVM);
