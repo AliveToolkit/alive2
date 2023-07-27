9f4d12e665e94e06a9eedbd844fb20a361333684
John McIver <john.mciver.iii@gmail.com>
2023-07-27 15:04:06 -0600

    Add include of cstdint to resolve definition of uint64_t
    This resolves a compilation error experienced on Fedora 38 due to lack of type
    definition.

--- tools/alive_lexer.h
+++ tools/alive_lexer.h
@@ -3,6 +3,7 @@
 // Copyright (c) 2018-present The Alive2 Authors.
 // Distributed under the MIT license that can be found in the LICENSE file.
 
+#include <cstdint>
 #include <string>
 #include <string_view>
 
