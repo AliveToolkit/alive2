c9030eaedd1e4cf8321d0e8dd8ede3566c758289
John Regehr <regehr@cs.utah.edu>
2023-09-25 16:04:25 -0600

    foo

--- tests/arm-tv/bugs/false-alarm-1.asminput.s
+++ tests/arm-tv/bugs/false-alarm-1.asminput.s
@@ -1,19 +1,22 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 13, 0
-	.globl	_f                              ; -- Begin function f
+	.text
+	.file	"false-alarm-1.asminput.ll"
+	.globl	f
 	.p2align	2
-_f:                                     ; @f
+	.type	f,@function
+f:
 	.cfi_startproc
-; %bb.0:
 	sxtb	w9, w0
 	mov	x8, #-196
-	cmn	w9, #14
-	csinv	x8, x8, xzr, le
+	cmn	w9, #13
+	csinv	x8, x8, xzr, lt
 	cmp	x8, #0
-	ccmn	w9, #13, #8, lt
-	cset	w8, lt
+	cset	w8, ge
+	cmn	w9, #13
+	csinc	w8, w8, wzr, ge
 	sbfx	x0, x8, #0, #1
 	ret
+.Lfunc_end0:
+	.size	f, .Lfunc_end0-f
 	.cfi_endproc
-                                        ; -- End function
-.subsections_via_symbols
+
+	.section	".note.GNU-stack","",@progbits
