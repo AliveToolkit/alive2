833d3255ba4b9d26942e2a12f621e892e8727a03
John Regehr <regehr@cs.utah.edu>
2023-09-25 15:51:31 -0600

    foo

--- tests/arm-tv/bugs/doubleword.asminput.s
+++ tests/arm-tv/bugs/doubleword.asminput.s
@@ -1,13 +1,15 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 13, 0
-	.globl	"_store-pre-indexed-doubleword2" ; -- Begin function store-pre-indexed-doubleword2
+	.text
+	.file	"doubleword.asminput.ll"
+	.globl	"store-pre-indexed-doubleword2"
 	.p2align	2
-"_store-pre-indexed-doubleword2":       ; @store-pre-indexed-doubleword2
+	.type	"store-pre-indexed-doubleword2",@function
+"store-pre-indexed-doubleword2":
 	.cfi_startproc
-; %bb.0:
 	ldr	x8, [x0]
 	str	xzr, [x8]
 	ret
+.Lfunc_end0:
+	.size	"store-pre-indexed-doubleword2", .Lfunc_end0-"store-pre-indexed-doubleword2"
 	.cfi_endproc
-                                        ; -- End function
-.subsections_via_symbols
+
+	.section	".note.GNU-stack","",@progbits
