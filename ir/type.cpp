e4508ba85747eb3a5e002915e544d2e08e751425
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2024-07-08 19:11:34 +0100

    fix llvm/llvm-project#97837: bug in the fptrunc nan payload

--- ir/type.cpp
+++ ir/type.cpp
@@ -469,16 +469,19 @@ expr FloatType::fromFloat(State &s, const expr &fp, const Type &from_type0,
 
   auto add_maybe_quiet = [&](const expr &e) {
     // account for fpext/fptruc
-    expr fraction = e.trunc(min(from_type->fractionBits(), fraction_bits));
-    if (fraction.bits() < fraction_bits)
+    auto orig_bw = from_type->fractionBits();
+    expr fraction = e.trunc(orig_bw);
+    expr truncated_is_nan = true;
+
+    if (orig_bw > fraction_bits) {
+      fraction = fraction.extract(orig_bw-1, orig_bw - fraction_bits);
+      truncated_is_nan = fraction.trunc(fraction_bits - 1) != 0;
+    } else if (orig_bw < fraction_bits) {
       fraction = fraction.concat_zeros(fraction_bits - fraction.bits());
+    }
     assert(!fraction.isValid() || fraction.bits() == fraction_bits);
 
-    expr truncated_is_nan = true;
-    if (from_type->fractionBits() > fraction_bits)
-      truncated_is_nan = fraction != 0;
-
-    expr qnan = fraction.extract(fraction_bits - 1, fraction_bits - 1);
+    expr qnan = fraction.sign();
     expr quieted = var.extract(0, 0);
     qnan = expr::mkIf(truncated_is_nan, qnan | quieted, expr::mkUInt(1, 1));
     exprs.add(qnan.concat(fraction.trunc(fraction_bits - 1)),
