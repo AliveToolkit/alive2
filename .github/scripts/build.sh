8cd21dce96cfc2ce1bb411ff3f90f2111d1b687f
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2025-04-21 20:59:22 +0100

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,8 +4,7 @@ mkdir build
 cd build
 
 if [[ "$RUNNER_OS" == "Linux" ]]; then
-  LLVM_VERSION="${CMAKE_CXX_COMPILER##*-}"   # extracts 20 or 21
-  LLVM_DIR="/usr/lib/llvm-${LLVM_VERSION}/lib/cmake"
+  LLVM_DIR="/usr/lib/llvm-21/lib/cmake/llvm"
 elif [[ "$RUNNER_OS" == "macOS" ]]; then
   LLVM_DIR="$(brew --prefix llvm)/lib/cmake/llvm"
 fi
@@ -13,7 +12,7 @@ fi
 cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
       -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER \
       -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS \
-      -DLLVM_DIR="$LLVM_DIR" \
+      -DCMAKE_PREFIX_PATH="$LLVM_DIR" \
       -DBUILD_TV=1 \
       -GNinja ..
 ninja
