1ff7306461053160ff3a479e7e3da0bf4643abca
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2025-04-21 19:37:02 +0100

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,5 +5,6 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
       -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER \
       -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS \
+      -DBUILD_TV=1 \
       -GNinja ..
 ninja
