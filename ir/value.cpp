677e63700d24862d8813db0403add5f8478a4b70
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2025-08-27 14:55:18 +0100

    Simplify byval pointer memory marking
    Updated memory marking for byval pointer to simplify the code.

--- ir/value.cpp
+++ ir/value.cpp
@@ -234,8 +234,7 @@ StateValue Input::mkInput(State &s, const Type &ty, unsigned child) const {
           .release();
     bool is_const = hasAttribute(ParamAttrs::NoWrite) ||
                     !s.getFn().getFnAttrs().mem.canWrite(MemoryAccess::Args);
-    // This byval ptr can never alias non-local memory blocks.
-    s.getMemory().markByValAndRecordLocalBlock(bid, is_const);
+    s.getMemory().markByVal(bid, is_const);
   } else {
     auto name = getSMTName(child);
     val = ty.mkInput(s, name.c_str(), attrs);
